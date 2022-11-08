7e994e3fa3a65d1e5149295db074abd129e4ff1e
sashmarins <sasha.marinskiiy@gmail.com>
2022-11-08 12:16:43 -0500

    finished seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# create Users
+25.times do |u|
+    name = Faker::Name.unique.name
+    user = User.create!(name: name)
+end
+
+# Create categories
+12.times do |c|
+    catname = Faker::Hobby.unique.activity
+    category = Category.create!(name: catname)
+end
+
+# Create Blogs
+(rand(20..40)).times do |b|
+    blogtitle = Faker::Verb.base
+    text = Faker::Lorem.paragraph
+    userid = rand(1..25)
+    catid = rand(1..12)
+    blog = Blog.create!(
+        title: blogtitle,
+        content: text,
+        user_id: userid,
+        category_id: catid
+    )
+end
